0e681e7e3c123c87428f96f35a02c8329b066fa1
Idevaldo <inetomec@yahoo.com.br>
2022-08-27 11:25:02 -0300

    feat: Criada classe para implementar a interface CozinhaRepository
    Aula 3.14. Conhecendo e implementando o padrão Repository

---
+++ src/main/java/com/algaworks/algafood/infraestructure/repository/CozinhaRepositoryImpl.java
@@ -0,0 +1,48 @@
+package com.algaworks.algafood.infraestructure.repository;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class CozinhaRepositoryImpl implements CozinhaRepository {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<Cozinha> listar() {
+        TypedQuery<Cozinha> query = manager.createQuery("from Cozinha", Cozinha.class);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public Cozinha buscar(Long id) {
+        Cozinha cozinha = manager.find(Cozinha.class, id);
+
+        return cozinha;
+    }
+
+    @Transactional
+    @Override
+    public Cozinha salvar(Cozinha cozinha) {
+        Cozinha cozinhaPersistida = manager.merge(cozinha);
+
+        return cozinhaPersistida;
+    }
+
+    @Transactional
+    @Override
+    public void remover(Cozinha cozinha) {
+        cozinha = buscar(cozinha.getId());
+
+        manager.remove(cozinha);
+    }
+}
