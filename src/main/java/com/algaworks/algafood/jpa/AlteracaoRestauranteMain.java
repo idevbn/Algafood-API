2677f912b7f0a6663dba9094d3bc70b6005e6c9a
Idevaldo <inetomec@yahoo.com.br>
2022-08-27 12:43:47 -0300

    feat: Adicionadas classes Main para teste das funcionalidades do repositório de Restaurante
    Aula 3.16. Desafio: Lombok e repositório de restaurantes

---
+++ src/main/java/com/algaworks/algafood/jpa/AlteracaoRestauranteMain.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood.jpa;
+
+import com.algaworks.algafood.AlgafoodApiApplication;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class AlteracaoRestauranteMain {
+    public static void main(String args[]) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        RestauranteRepository restauranteRepository = applicationContext.getBean(RestauranteRepository.class);
+
+        Restaurante restaurante = new Restaurante();
+        restaurante.setId(2L);
+        restaurante.setNome("Tokyo - Comida Japonesa");
+
+        restauranteRepository.salvar(restaurante);
+
+        System.out.printf("%d - %s\n", restaurante.getId(), restaurante.getNome());
+    }
+}
