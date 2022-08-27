71a2289cd023b647b4181d8c45101243d4d5e0fb
Idevaldo <inetomec@yahoo.com.br>
2022-08-27 13:48:32 -0300

    chore: Alterados valores de inserção para tb_restaurante, acrescentando o id da cozinha
    Aula 3.17. Mapeando relacionamento com @ManyToOne

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,6 @@
 INSERT INTO tb_cozinha (nome) VALUES ('Tailandesa');
 INSERT INTO tb_cozinha (nome) VALUES ('Indiana');
-INSERT INTO  tb_restaurante (nome, taxa_frete) VALUES ('Sabor Brasileiro', 11.50);
-INSERT INTO  tb_restaurante (nome, taxa_frete) VALUES ('Kyoto - Comida Japonesa', 9.80);
+
+INSERT INTO  tb_restaurante (nome, taxa_frete, cozinha_id) VALUES ('Pad Thai', 11.50, 1);
+INSERT INTO  tb_restaurante (nome, taxa_frete, cozinha_id) VALUES ('Bistrô Vadapav', 9.80, 2);
+INSERT INTO  tb_restaurante (nome, taxa_frete, cozinha_id) VALUES ('Namaskar', 7.39, 2);
