650b3ddb4ca36a2164f65e760bd7e810a7844a78
Idevaldo <inetomec@yahoo.com.br>
2022-08-26 23:28:50 -0300

    feat: Adicionado método para criação de uma cozinha
    Aula 3.9. Adicionando um objeto no banco de dados

--- src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
+++ src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.jpa;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -18,5 +19,12 @@ public class CadastroCozinha {
         TypedQuery<Cozinha> query = manager.createQuery("from Cozinha", Cozinha.class);
 
         return query.getResultList();
+    }
+
+    @Transactional
+    public Cozinha adicionar(Cozinha cozinha) {
+        Cozinha cozinhaPersistida = manager.merge(cozinha);
+
+        return cozinhaPersistida;
     }
 }
