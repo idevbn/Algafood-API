f84b7e3460396e1f1452f1625182f4f61c1f4607
Idevaldo <inetomec@yahoo.com.br>
2022-08-27 12:48:01 -0300

    chore: Adicionados valores para a tabela tb_restaurante no arquivo import.sql
    Aula 3.16. Desafio: Lombok e repositório de restaurantes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
 INSERT INTO tb_cozinha (nome) VALUES ('Tailandesa');
 INSERT INTO tb_cozinha (nome) VALUES ('Indiana');
+INSERT INTO  tb_restaurante (nome) VALUES ('Sabor Brasileiro');
+INSERT INTO  tb_restaurante (nome) VALUES ('Kyoto - Comida Japonesa');
