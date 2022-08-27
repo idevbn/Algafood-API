c6084ef01233774aefd632437c1a0fee70c0bcea
Idevaldo <inetomec@yahoo.com.br>
2022-08-27 12:45:11 -0300

    refactor: Refatorada classe Restaurante para utilizar as anotações do projeto Lombok
    Aula 3.16. Desafio: Lombok e repositórios de restaurantes

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,13 +1,18 @@
 package com.algaworks.algafood.domain.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.Objects;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "tb_restaurante")
 public class Restaurante {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -15,41 +20,4 @@ public class Restaurante {
     private String nome;
 
     private BigDecimal taxaFrete;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public BigDecimal getTaxaFrete() {
-        return taxaFrete;
-    }
-
-    public void setTaxaFrete(BigDecimal taxaFrete) {
-        this.taxaFrete = taxaFrete;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Restaurante that = (Restaurante) o;
-        return id.equals(that.id) && nome.equals(that.nome) && taxaFrete.equals(that.taxaFrete);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, nome, taxaFrete);
-    }
 }
