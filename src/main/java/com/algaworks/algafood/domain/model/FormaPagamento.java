a8200e9f49bf38dfcee409f4fdce5b807a4eb254
Idevaldo <inetomec@yahoo.com.br>
2022-08-29 20:55:21 -0300

    feat: Criada entidade FormaPagamento
    Aula 3.20. Desafio: mapenado entidades

---
+++ src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_forma_pagamento")
+public class FormaPagamento {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurante_id")
+    private Restaurante restaurante;
+}
