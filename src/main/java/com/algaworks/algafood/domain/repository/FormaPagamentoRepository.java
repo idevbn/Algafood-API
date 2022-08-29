512d3356745fecb9b49805cec880bc7c60a15344
Idevaldo <inetomec@yahoo.com.br>
2022-08-29 20:56:18 -0300

    feat: Criado repositório da entidade FormaPagamento
    Aula 3.20. Desafio: mapeando entidades

---
+++ src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
@@ -0,0 +1,10 @@
+package com.algaworks.algafood.domain.repository;
+
+import com.algaworks.algafood.domain.model.FormaPagamento;
+
+import java.util.List;
+
+public interface FormaPagamentoRepository {
+    List<FormaPagamento> listar();
+    FormaPagamento buscar(Long id);
+}
