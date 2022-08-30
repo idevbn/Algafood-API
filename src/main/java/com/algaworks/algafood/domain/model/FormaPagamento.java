aa6098a7c728f1bd415c5883a6d19f85800e6df9
Idevaldo <inetomec@yahoo.com.br>
2022-08-29 23:25:24 -0300

    refactor: Adição da anotação @Column(nullable=false) no atributo descricao da entidade FormaPagamento
    Aula 3.20. Desafio: mapeando entidades

--- src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
+++ src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
@@ -16,6 +16,7 @@ public class FormaPagamento {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String descricao;
 
     @ManyToOne
