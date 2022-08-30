6d9c489f5514e56fc5d6a0af7a8674f33c25368f
Idevaldo <inetomec@yahoo.com.br>
2022-08-29 22:48:41 -0300

    feat: Criada classe CidadeRepositoryImpl que implementa a classe CidadeRepository
    Aula 3.20. Desafio: mapeando entidades

---
+++ src/main/java/com/algaworks/algafood/infraestructure/repository/CidadeRepositoryImpl.java
@@ -0,0 +1,46 @@
+package com.algaworks.algafood.infraestructure.repository;
+
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.repository.CidadeRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class CidadeRepositoryImpl implements CidadeRepository {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<Cidade> listar() {
+        TypedQuery<Cidade> query = manager.createQuery("from Cidade", Cidade.class);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public Cidade buscar(Long id) {
+        Cidade cidade = manager.find(Cidade.class, id);
+
+        return cidade;
+    }
+
+    @Transactional
+    @Override
+    public Cidade salvar(Cidade cidade) {
+        Cidade cidadePersistida = manager.merge(cidade);
+
+        return cidadePersistida;
+    }
+
+    @Transactional
+    @Override
+    public void remover(Cidade cidade) {
+        cidade = buscar(cidade.getId());
+
+        manager.remove(cidade);
+    }
+}
