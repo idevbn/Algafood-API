ad0da2fc310bf6da89e345c7447ad532ed04068d
Idevaldo <inetomec@yahoo.com.br>
2022-08-29 22:36:08 -0300

    refactor: Adicionados valores na tabela tb_estado
    Aula 3.20. Desafio: mapeando entidades

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,3 +8,8 @@ INSERT INTO  tb_restaurante (id, nome, taxa_frete, cozinha_id) VALUES (3, 'Namas
 INSERT INTO tb_forma_pagamento (id, descricao, restaurante_id) VALUES (1, 'Cartão de crédito', 1);
 INSERT INTO tb_forma_pagamento (id, descricao, restaurante_id) VALUES (2, 'Cartão de débito', 2);
 INSERT INTO tb_forma_pagamento (id, descricao, restaurante_id) VALUES (3, 'Pix', 3);
+
+INSERT INTO tb_estado (nome) VALUES ('PB');
+INSERT INTO tb_estado (nome) VALUES ('RN');
+INSERT INTO tb_estado (nome) VALUES ('CE');
+INSERT INTO tb_estado (nome) VALUES ('PE');
