15433088b418f735af13d06f0043c9914eed33f0
Idevaldo <inetomec@yahoo.com.br>
2022-08-29 21:12:09 -0300

    feat: Adicionados métodos salvar e remover no repositório FormaPagamentoRepository
    Aula 3.20. Desafio: mapeando entidades

--- src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface FormaPagamentoRepository {
     List<FormaPagamento> listar();
     FormaPagamento buscar(Long id);
+    FormaPagamento salvar(FormaPagamento formaPagamento);
+    void remover(FormaPagamento formaPagamento);
 }
