b92a182d13dd55cfce6e19eb7d7dc78115cfb129
Idevaldo <inetomec@yahoo.com.br>
2022-08-29 22:57:20 -0300

    feat: Adicionada classe Main para teste de funcionalidade de consulta das Cidades
    Aula 3.20. Desafio: mapeando entidades

---
+++ src/main/java/com/algaworks/algafood/jpa/ConsultaCidadeMain.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.jpa;
+
+import com.algaworks.algafood.AlgafoodApiApplication;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.Estado;
+import com.algaworks.algafood.domain.repository.CidadeRepository;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class ConsultaCidadeMain {
+    public static void main(String args[]) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CidadeRepository cidadeRepository = applicationContext.getBean(CidadeRepository.class);
+
+        List<Cidade> cidades = cidadeRepository.listar();
+
+        for (Cidade cidade : cidades) {
+            System.out.printf("Cidade: %s - Estado: %s \n",
+                    cidade.getNome(),
+                    cidade.getEstado().getNome()
+            );
+        }
+    }
+}
