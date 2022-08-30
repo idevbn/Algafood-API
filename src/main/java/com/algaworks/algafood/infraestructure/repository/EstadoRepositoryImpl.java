20e103fcc59bccb85ea62b9f1e3815792350656a
Idevaldo <inetomec@yahoo.com.br>
2022-08-29 22:26:00 -0300

    feat: Definida classe de implementação EstadoRepositoryImpl
    Aula 3.20. Desafio: mapeando entidades

---
+++ src/main/java/com/algaworks/algafood/infraestructure/repository/EstadoRepositoryImpl.java
@@ -0,0 +1,50 @@
+package com.algaworks.algafood.infraestructure.repository;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Estado;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class EstadoRepositoryImpl implements EstadoRepository {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<Estado> listar() {
+        TypedQuery<Estado> query = manager.createQuery("from Estado", Estado.class);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public Estado buscar(Long id) {
+        Estado estado = manager.find(Estado.class, id);
+
+        return estado;
+    }
+
+    @Transactional
+    @Override
+    public Estado salvar(Estado estado) {
+        Estado estadoPersistido = manager.merge(estado);
+
+        return estadoPersistido;
+    }
+
+    @Transactional
+    @Override
+    public void remover(Estado estado) {
+        estado = buscar(estado.getId());
+
+        manager.remove(estado);
+    }
+}
