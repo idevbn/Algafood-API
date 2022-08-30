a5c70371463746741d29930d272b1afaf7c4c12c
Idevaldo <inetomec@yahoo.com.br>
2022-08-29 22:08:47 -0300

    refactor: Adicionados valores de inserção para a tabelas tb_restaurante e tb_forma_pagamento
    Aula 3.20. Desafio: mapeando entidades

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,10 @@
 INSERT INTO tb_cozinha (nome) VALUES ('Tailandesa');
 INSERT INTO tb_cozinha (nome) VALUES ('Indiana');
 
-INSERT INTO  tb_restaurante (nome, taxa_frete, cozinha_id) VALUES ('Pad Thai', 11.50, 1);
-INSERT INTO  tb_restaurante (nome, taxa_frete, cozinha_id) VALUES ('Bistrô Vadapav', 9.80, 2);
-INSERT INTO  tb_restaurante (nome, taxa_frete, cozinha_id) VALUES ('Namaskar', 7.39, 2);
+INSERT INTO  tb_restaurante (id, nome, taxa_frete, cozinha_id) VALUES (1, 'Pad Thai', 11.50, 1);
+INSERT INTO  tb_restaurante (id, nome, taxa_frete, cozinha_id) VALUES (2, 'Bistrô Vadapav', 9.80, 2);
+INSERT INTO  tb_restaurante (id, nome, taxa_frete, cozinha_id) VALUES (3, 'Namaskar', 7.39, 2);
+
+INSERT INTO tb_forma_pagamento (id, descricao, restaurante_id) VALUES (1, 'Cartão de crédito', 1);
+INSERT INTO tb_forma_pagamento (id, descricao, restaurante_id) VALUES (2, 'Cartão de débito', 2);
+INSERT INTO tb_forma_pagamento (id, descricao, restaurante_id) VALUES (3, 'Pix', 3);
