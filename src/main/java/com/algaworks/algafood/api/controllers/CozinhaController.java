e35eb9d6616e417fa688ee9d61eedbb81d67acc1
Idevaldo <inetomec@yahoo.com.br>
2022-09-03 16:37:52 -0300

    feat: Criado o controlador responsável por listar as cozinhas
    Aula 4.10. Modelando e requisitando um Collection Resource com GET

---
+++ src/main/java/com/algaworks/algafood/api/controllers/CozinhaController.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood.api.controllers;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository repository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        List<Cozinha> cozinhas = this.repository.listar();
+
+        return cozinhas;
+    }
+}
