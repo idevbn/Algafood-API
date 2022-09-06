d83c373a48f0c97575d53b9e4b95168306e30463
Idevaldo <inetomec@yahoo.com.br>
2022-09-06 20:21:03 -0300

    refactor: Removido content negotiation para listar cozinhas

--- src/main/java/com/algaworks/algafood/api/controllers/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controllers/CozinhaController.java
@@ -3,7 +3,6 @@ package com.algaworks.algafood.api.controllers;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping(value = "/cozinhas")
 public class CozinhaController {
 
     @Autowired
     private CozinhaRepository repository;
 
-    @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    @GetMapping
     public List<Cozinha> listar() {
         List<Cozinha> cozinhas = this.repository.listar();
 
