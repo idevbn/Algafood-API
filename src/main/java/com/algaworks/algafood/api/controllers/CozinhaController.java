2d43186702b06efad319e385e71efc9be79581be
Idevaldo <inetomec@yahoo.com.br>
2022-09-07 10:56:53 -0300

    refactor: Devolvida a resposta à requisição de busca de uma cozinha pelo id, utilizando o ResponseEntity
    Aula 4.20. Manipulando a resposta HTTP com ResponseEntity

--- src/main/java/com/algaworks/algafood/api/controllers/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controllers/CozinhaController.java
@@ -3,6 +3,8 @@ package com.algaworks.algafood.api.controllers;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,10 +26,10 @@ public class CozinhaController {
         return cozinhas;
     }
 
-    @GetMapping(value = "{id}")
-    public Cozinha buscar(@PathVariable(value = "id") Long id) throws Exception {
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Cozinha> buscar(@PathVariable(value = "id") Long id) {
         Cozinha cozinha = this.repository.buscar(id);
 
-        return cozinha;
+        return ResponseEntity.status(HttpStatus.OK).body(cozinha);
     }
 }
