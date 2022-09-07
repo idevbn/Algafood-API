a030df207ec4d8b3e218590fc41b6b02cf456319
Idevaldo <inetomec@yahoo.com.br>
2022-09-07 11:55:02 -0300

    feat: Criado o método POST reponsável pelo cadastro de uma nova cozinha
    Aula 4.23. Modelando e implementando a inclusão de recursos com POST

--- src/main/java/com/algaworks/algafood/api/controllers/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controllers/CozinhaController.java
@@ -5,10 +5,7 @@ import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,5 +37,16 @@ public class CozinhaController {
         ResponseEntity<Cozinha> cozinhaResposta = ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 
         return cozinhaResposta;
+    }
+
+    @PostMapping
+    public ResponseEntity<Cozinha> adicionar(@RequestBody Cozinha cozinha) {
+        Cozinha cozinhaSalva = this.repository.salvar(cozinha);
+
+        ResponseEntity<Cozinha> cozinhaResponse = ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(cozinhaSalva);
+
+        return cozinhaResponse;
     }
 }
