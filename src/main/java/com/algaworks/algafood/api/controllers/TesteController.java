29bc4550acad83b10d117f1a37b7fcbdca68e975
Idevaldo <inetomec@yahoo.com.br>
2022-09-20 20:33:05 -0300

    feat: Implementados métodos que utilizam query methods do Spring Data JPA
    Aula 5.7. Usando as keywords para definir critérios de query methods

--- src/main/java/com/algaworks/algafood/api/controllers/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controllers/TesteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algafood.api.controllers;
 
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,9 +23,12 @@ public class TesteController {
     @Autowired
     private CozinhaRepository repository;
 
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
     @GetMapping(value = "/cozinhas/por-nome")
     public ResponseEntity<List<Cozinha>> cozinhasPorNome(@RequestParam String nome) {
-        List<Cozinha> cozinhas = this.repository.findTodasByNome(nome);
+        List<Cozinha> cozinhas = this.repository.findTodasByNomeContaining(nome);
 
         ResponseEntity<List<Cozinha>> cozinhasResponse = ResponseEntity
                 .status(HttpStatus.OK)
@@ -40,5 +46,35 @@ public class TesteController {
                 .body(cozinha);
 
         return cozinhaResponse;
+    }
+
+    @GetMapping(value = "/restaurantes/por-taxa-frete")
+    public ResponseEntity<List<Restaurante>> restaurantesPorTaxaFrete(
+            @RequestParam BigDecimal taxaInicial,
+            @RequestParam BigDecimal taxaFinal
+    ) {
+        List<Restaurante> restaurantes = this.restauranteRepository
+                .findByTaxaFreteBetween(taxaInicial, taxaFinal);
+
+        ResponseEntity<List<Restaurante>> restaurantesResponse = ResponseEntity
+                .status(HttpStatus.OK)
+                .body(restaurantes);
+
+        return restaurantesResponse;
+    }
+
+    @GetMapping(value = "/restaurantes/por-nome")
+    public ResponseEntity<List<Restaurante>> restaurantesPorNome(
+            @RequestParam String nome,
+            @RequestParam Long cozinhaId
+    ) {
+        List<Restaurante> restaurantes = this.restauranteRepository
+                .findByNomeContainingAndCozinhaId(nome, cozinhaId);
+
+        ResponseEntity<List<Restaurante>> restaurantesResponse = ResponseEntity
+                .status(HttpStatus.OK)
+                .body(restaurantes);
+
+        return restaurantesResponse;
     }
 }
