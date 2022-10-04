8ab382a82825af44dee0dfcfef01eac478c7be1c
Idevaldo <inetomec@yahoo.com.br>
2022-10-03 23:23:37 -0300

    refactor: Criada consulta dinâmica utilizando o Criteria API
    Aula 5.15. Tornando a consulta com Criteria API com filtros dinâmicos

--- src/main/java/com/algaworks/algafood/infraestructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infraestructure/repository/RestauranteRepositoryImpl.java
@@ -3,15 +3,13 @@ package com.algaworks.algafood.infraestructure.repository;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQuery;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -26,27 +24,29 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQuery {
             BigDecimal taxaFreteInicial,
             BigDecimal taxaFreteFinal
     ) {
-        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        var builder = manager.getCriteriaBuilder();
 
-        CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+        var criteria = builder.createQuery(Restaurante.class);
+        var root = criteria.from(Restaurante.class);
 
-        Root<Restaurante> root = criteria.from(Restaurante.class);
+        var predicates = new ArrayList<Predicate>();
 
-        Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
+        if (StringUtils.hasText(nome)) {
+            predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+        }
 
-        Predicate taxaInicialPredicate = builder
-                .greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+        if (taxaFreteInicial != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+        }
 
-        Predicate taxaFinalPredicate = builder
-                .lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+        if (taxaFreteFinal != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+        }
 
-        criteria.where(nomePredicate);
+        criteria.where(predicates.toArray(new Predicate[0]));
 
-        TypedQuery<Restaurante> query = manager
-                .createQuery(criteria);
-
-        List<Restaurante> restauranteList = query.getResultList();
-
-        return restauranteList;
+        var query = manager.createQuery(criteria);
+        var resultList = query.getResultList();
+        return resultList;
     }
 }
