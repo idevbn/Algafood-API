20567fb6c1bff00e83e7d13a426e21e58eacb01c
Idevaldo Neto <inetomec@yahoo.com.br>
2022-12-29 11:39:56 -0300

    refactor: Refatorado o código para acrescer propriedades da resposta do erro em conformidade com a RFC-7807
    Aula 8.18. Padronizando o formato de problemas no corpo de respostas com a RFC 7807

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiError.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiError.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,9 +11,12 @@ import java.time.LocalDateTime;
  */
 @Getter
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiError {
 
-    private LocalDateTime dataHora;
-    private String mensagem;
+    private Integer status;
+    private String type;
+    private String title;
+    private String detail;
 
 }
