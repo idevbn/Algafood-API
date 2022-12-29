da01c797ee3915dc96eb602160ea497cef83ff0f
Idevaldo Neto <inetomec@yahoo.com.br>
2022-12-29 12:46:55 -0300

    refactor: Adicionada mensagem de erro para ser utilizada no caso de uma HttpMessageNotReadableException
    Aula 8.20. Customizando exception handlers de ResponseEntityExceptionHandler

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiErrorType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiErrorType.java
@@ -10,7 +10,8 @@ public enum ApiErrorType {
 
     ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
     ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
-    ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
+    ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
+    MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível");
 
     private String title;
     private String uri;
