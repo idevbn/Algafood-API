3dc7271315103de4fdf970a25610505b4839e086
Idevaldo Neto <inetomec@yahoo.com.br>
2023-02-01 23:25:45 -0300

    feat: Criada anotação ValorZeroIncluiDescricao e implementação ValorZeroIncluiDescricaoValidator para validar de maneira customizada em nível de classe
    Aula 9.17. Criando constraints de validação customizadas em nível de classe

---
+++ src/main/java/com/algaworks/algafood/core/validation/ValorZeroIncluiDescricao.java
@@ -0,0 +1,45 @@
+package com.algaworks.algafood.core.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Anotação utilizada em nível de classe cuja implementação {@link ValorZeroIncluiDescricaoValidator}
+ * valida se, para o caso do atributo taxaFrete ter o valor 0, a descrição (nome) p
+ * assada equivale ao valor atribuído no campo descricaoObrigatoria.
+ * <br />
+ *
+ * EXEMPLO:
+ *
+ * <br />
+ * {{@code @ValorZeroIncluiDescricao(valorField} = "taxaFrete",
+ *         descricaoField = "nome", descricaoObrigatoria = "Frete Grátis")
+ * public class Restaurante {}
+ * <br />
+ *
+ * Caso uma requisição seja feita, e o valor passado no atributo "nome" do corpo não corresponda
+ * ao atributo descricaoObrigatoria = "Frete Grátis", será disparado um erro de validação.
+ *
+ */
+@Target({ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {ValorZeroIncluiDescricaoValidator.class})
+public @interface ValorZeroIncluiDescricao {
+
+    String message() default "{ValorZeroIncluiDescricao}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    String valorField();
+
+    String descricaoField();
+
+    String descricaoObrigatoria();
+
+}
---
+++ src/main/java/com/algaworks/algafood/core/validation/ValorZeroIncluiDescricaoValidator.java
@@ -0,0 +1,50 @@
+package com.algaworks.algafood.core.validation;
+
+import org.springframework.beans.BeanUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.ValidationException;
+import java.math.BigDecimal;
+
+public class ValorZeroIncluiDescricaoValidator implements
+        ConstraintValidator<ValorZeroIncluiDescricao, Object> {
+
+    private String valorField;
+    private String descricaoField;
+    private String descricaoObrigatoria;
+
+    @Override
+    public void initialize(final ValorZeroIncluiDescricao constraint) {
+        this.valorField = constraint.valorField();
+        this.descricaoField = constraint.descricaoField();
+        this.descricaoObrigatoria = constraint.descricaoObrigatoria();
+    }
+
+    @Override
+    public boolean isValid(final Object objValidacao, final ConstraintValidatorContext context) {
+        boolean valido = true;
+
+        try {
+             final BigDecimal valor = (BigDecimal) BeanUtils
+                     .getPropertyDescriptor(objValidacao.getClass(), valorField)
+                    .getReadMethod()
+                    .invoke(objValidacao);
+
+            final String descricao = (String) BeanUtils
+                    .getPropertyDescriptor(objValidacao.getClass(), descricaoField)
+                    .getReadMethod()
+                    .invoke(objValidacao);
+
+            if (valor != null && BigDecimal.ZERO.compareTo(valor) == 0 && descricao != null) {
+                valido = descricao.toLowerCase().contains(this.descricaoObrigatoria.toLowerCase());
+            }
+
+            return valido;
+
+        } catch (final Exception e) {
+            throw new ValidationException(e);
+        }
+    }
+
+}
