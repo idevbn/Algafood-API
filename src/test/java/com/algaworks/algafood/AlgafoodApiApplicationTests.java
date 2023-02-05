5a16ea4872dfef41e77515d83d01d9309fcdb0b8
Idevaldo Neto <inetomec@yahoo.com.br>
2023-02-04 21:55:47 -0300

    test: Criados testes iniciais para o cadastro de uma nova cozinha, e o cadastro de uma cozinha sem nome
    Aula 10.3. Criando e rodando um teste de integração com Spring Boot, JUnit e AssertJ

--- src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
+++ src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
@@ -1,16 +1,52 @@
 package com.algaworks.algafood;
 
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.validation.ConstraintViolationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-class AlgafoodApiApplicationTests {
+class CadastroCozinhaIntegrationTest {
+
+	private final CadastroCozinhaService cozinhaService;
+
+	@Autowired
+	 CadastroCozinhaIntegrationTest(final CadastroCozinhaService cozinhaService) {
+		this.cozinhaService = cozinhaService;
+	}
 
 	@Test
-	void contextLoads() {
+	public void testarCadastroCozinhaComSucesso() {
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome("Chinesa");
+
+		novaCozinha = cozinhaService.salvar(novaCozinha);
+
+		assertThat(novaCozinha).isNull();
+		assertThat(novaCozinha.getId()).isNotNull();
+	}
+
+	@Test
+	public void testarCadastroCozinhaSemNome() {
+		final Cozinha novaCozinha = new Cozinha();
+
+		novaCozinha.setNome(null);
+
+		final ConstraintViolationException erroEsperado =
+				Assertions.assertThrows(ConstraintViolationException.class, () -> {
+					this.cozinhaService.salvar(novaCozinha);
+				});
+
+		assertThat(erroEsperado).isNotNull();
 	}
 
 }
