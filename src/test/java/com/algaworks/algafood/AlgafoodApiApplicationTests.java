29a4e2115d28c37fe5d3d5821f9bb29c5b0da886
Idevaldo Neto <inetomec@yahoo.com.br>
2023-02-24 20:48:18 -0300

    test: WIP: Criado teste que valida o corpo da resposta ao listar as cozinhas no banco
    Aula 10.9. Validando o corpo da resposta HTTP

--- src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
+++ src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
@@ -12,10 +12,14 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static io.restassured.RestAssured.enableLoggingOfRequestAndResponseIfValidationFails;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CadastroCozinhaIT {
+
+	private static final String COZINHAS = "/cozinhas";
 
 	private final CadastroCozinhaService cozinhaService;
 
@@ -32,13 +36,28 @@ class CadastroCozinhaIT {
 		enableLoggingOfRequestAndResponseIfValidationFails();
 
 		given()
-				.basePath("/cozinhas")
-				.port(port)
+				.basePath(COZINHAS)
+				.port(this.port)
 				.accept(ContentType.JSON)
 				.when()
 				.get()
 				.then()
 				.statusCode(HttpStatus.OK.value());
+	}
+
+	@Test
+	public void deveConter2Cozinhas_QuandoRetornarCozinhas() {
+		enableLoggingOfRequestAndResponseIfValidationFails();
+
+		given()
+				.basePath(COZINHAS)
+				.port(this.port)
+				.accept(ContentType.JSON)
+				.when()
+				.get()
+				.then()
+				.body("", hasSize(2))
+				.body("nome", hasItems("Indiana", "Tailandesa"));
 	}
 
 }
