5861aabf8be92cd47100de8fd436cf9c558c0221
Idevaldo Neto <inetomec@yahoo.com.br>
2023-03-05 11:38:09 -0300

    feat: Criados Models de Cozinha e Restaurante utilizando do padrão DTO para serem retornados pela API
    Aula 11.9. Isolando o Domain Model do Representation Model com o padrão DTO

---
+++ src/main/java/com/algaworks/algafood/api/model/RestauranteModel.java
@@ -0,0 +1,12 @@
+package com.algaworks.algafood.api.model;
+
+import java.math.BigDecimal;
+
+public class RestauranteModel {
+
+    private Long id;
+    private String nome;
+    private BigDecimal taxaFrete;
+    private CozinhaModel cozinha;
+
+}
