45f66d66696257c2484dca2025b37f73ec8e0a81
Idevaldo Neto <inetomec@yahoo.com.br>
2023-03-12 23:51:15 -0300

    refactor: Alterado o método que salva um Restaurante, recuperando a cidade e atribuindo o valor da mesma ao Restaurante antes do momento de salvá-lo
    Aula 12.7. Refatorando serviço de cadastro de restaurante para incluir endereço

--- src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.service;
 
 import com.algaworks.algafood.domain.exception.RestauranteNaoEncontradoException;
+import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
@@ -14,21 +15,28 @@ public class CadastroRestauranteService {
 
     private final RestauranteRepository repository;
     private final CadastroCozinhaService cozinhaService;
+    private final CadastroCidadeService cidadeService;
 
     @Autowired
     public CadastroRestauranteService(final RestauranteRepository repository,
-                                      final CadastroCozinhaService cozinhaService) {
+                                      final CadastroCozinhaService cozinhaService,
+                                      final CadastroCidadeService cidadeService) {
         this.repository = repository;
         this.cozinhaService = cozinhaService;
+        this.cidadeService = cidadeService;
     }
 
     @Transactional
     public Restaurante salvar(final Restaurante restaurante) {
         final Long cozinhaId = restaurante.getCozinha().getId();
+        final Long cidadeId = restaurante.getEndereco().getCidade().getId();
 
         final Cozinha cozinha = this.cozinhaService.buscarOuFalhar(cozinhaId);
 
+        final Cidade cidade = this.cidadeService.buscarOuFalhar(cidadeId);
+
         restaurante.setCozinha(cozinha);
+        restaurante.getEndereco().setCidade(cidade);
 
         final Restaurante restauranteSalvo = this.repository.save(restaurante);
 
