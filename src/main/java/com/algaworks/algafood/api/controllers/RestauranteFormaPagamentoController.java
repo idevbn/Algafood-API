542a5e0f9084ebaffb610c1175322c059d093a04
Idevaldo Neto <inetomec@yahoo.com.br>
2023-03-14 17:57:53 -0300

    feat: Criado controlador responsável pelas requisições das formas de pagamento associadas a um Restaurate
    Aula 12.12. Implementando os endpoints de associação de formas de pagamento em restaurantes

---
+++ src/main/java/com/algaworks/algafood/api/controllers/RestauranteFormaPagamentoController.java
@@ -0,0 +1,78 @@
+package com.algaworks.algafood.api.controllers;
+
+import com.algaworks.algafood.api.assembler.FormaPagamentoOutputDTOAssembler;
+import com.algaworks.algafood.api.model.out.FormaPagamentoOutputDTO;
+import com.algaworks.algafood.domain.model.FormaPagamento;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping(value = "/restaurantes/{id}/formas-pagamento")
+public class RestauranteFormaPagamentoController {
+
+    private final CadastroRestauranteService service;
+    private final FormaPagamentoOutputDTOAssembler assembler;
+
+    @Autowired
+    public RestauranteFormaPagamentoController(final CadastroRestauranteService service,
+                                               final FormaPagamentoOutputDTOAssembler assembler) {
+        this.service = service;
+        this.assembler = assembler;
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<FormaPagamentoOutputDTO>> listar(
+            @PathVariable("id") final Long id
+    ) {
+        final Restaurante restauranteEncontrado = this.service.buscarOuFalhar(id);
+
+        final Collection<FormaPagamento> formasPagamento = restauranteEncontrado
+                .getFormasPagamento();
+
+        final List<FormaPagamentoOutputDTO> formasPagamentoOutputDTOS = this.assembler
+                .toCollectionModel(formasPagamento);
+
+        final ResponseEntity<List<FormaPagamentoOutputDTO>> response = ResponseEntity
+                .status(HttpStatus.OK)
+                .body(formasPagamentoOutputDTOS);
+
+        return response;
+    }
+
+    @PutMapping(value = "/{formaPagamentoId}")
+    public ResponseEntity<Void> associar(
+            @PathVariable("id") final Long id,
+            @PathVariable("formaPagamentoId") final Long formaPagamentoId
+    ) {
+        this.service.associarFormaPagamento(id, formaPagamentoId);
+
+        final ResponseEntity<Void> response = ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
+
+        return response;
+    }
+
+    @DeleteMapping(value = "/{formaPagamentoId}")
+    public ResponseEntity<Void> desassociar(
+            @PathVariable("id") final Long id,
+            @PathVariable("formaPagamentoId") final Long formaPagamentoId
+    ) {
+       this.service.desassociarFormaPagamento(id, formaPagamentoId);
+
+        final ResponseEntity<Void> response = ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
+
+        return response;
+    }
+
+}
