cd0f18054f64721460cf963daed4cac36ac33463
Idevaldo Neto <inetomec@yahoo.com.br>
2023-03-14 17:54:23 -0300

    feat: Criados métodos que adicionam e removem a forma de pagamento na entidade Restaurante
    O atributo formasPagamento foi alterado de List para Set para evitar valores duplicados
    
    Aula 12.12. Implementando os endpoints de associação de formas de pagamento em restaurantes

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -10,7 +10,9 @@ import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @Entity
@@ -54,7 +56,7 @@ public class Restaurante {
             joinColumns = @JoinColumn(name = "restaurante_id"),
             inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id")
     )
-    private List<FormaPagamento> formasPagamento = new ArrayList<>();
+    private Set<FormaPagamento> formasPagamento = new HashSet<>();
 
     @OneToMany(mappedBy = "restaurante")
     private List<Produto> produtos = new ArrayList<>();
@@ -65,6 +67,14 @@ public class Restaurante {
 
     public void inativar() {
         setAtivo(false);
+    }
+
+    public boolean removerFormaPagamento(final FormaPagamento formaPagamento) {
+        return this.getFormasPagamento().remove(formaPagamento);
+    }
+
+    public boolean adicionarFormaPagamento(final FormaPagamento formaPagamento) {
+        return this.getFormasPagamento().add(formaPagamento);
     }
 
 }
