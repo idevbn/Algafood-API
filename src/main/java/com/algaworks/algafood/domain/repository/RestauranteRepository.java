d5cbeedb9015b0c4fa600c638ec9a8cf1549e43d
Idevaldo Neto <inetomec@yahoo.com.br>
2023-03-14 20:03:39 -0300

    refactor: Adicionada a anotação @Repository nos repositórios
    Alteração realizada após a aula 12.13
    
    Aula 12.13. Desafio: implementando os endpoints de produtos

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -3,11 +3,13 @@ package com.algaworks.algafood.domain.repository;
 import com.algaworks.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>,
         RestauranteRepositoryQuery, JpaSpecificationExecutor<Restaurante> {
     List<Restaurante> findByTaxaFreteBetween(final BigDecimal taxaInicial,
