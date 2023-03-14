63a94fdb55282b83be12fd5c25fa133885975070
Idevaldo Neto <inetomec@yahoo.com.br>
2023-03-14 19:19:04 -0300

    feat: Criados o repositório e o service para a entidade do Produto
    Aula 12.13. Desafio: implementando os endpoints de produtos

---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroProdutoService.java
@@ -0,0 +1,35 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.exception.ProdutoNaoEncontradoException;
+import com.algaworks.algafood.domain.model.Produto;
+import com.algaworks.algafood.domain.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class CadastroProdutoService {
+
+    private final ProdutoRepository repository;
+
+    @Autowired
+    public CadastroProdutoService(final ProdutoRepository repository) {
+        this.repository = repository;
+    }
+
+    public Produto buscarOuFalhar(final Long id, final Long restauranteId) {
+        final Produto produtoEncontrado = this.repository.findById(id, restauranteId)
+                .orElseThrow(() -> new ProdutoNaoEncontradoException(id, restauranteId));
+
+        return produtoEncontrado;
+    }
+
+    @Transactional
+    public Produto salvar(final Produto produto) {
+        final Produto produtoSalvo = this.repository.save(produto);
+
+        return produtoSalvo;
+    }
+
+}
