ba31bbce6803fc89044cc0884a26480add5e76f1
Idevaldo Neto <inetomec@yahoo.com.br>
2023-03-15 07:31:54 -0300

    feat: Criado DTO de saída exibido quando uma Permissao é listada
    Aula 12.15. Desafio: implementando os endpoints de associação de grupos com permissões

---
+++ src/main/java/com/algaworks/algafood/api/model/out/PermissaoOutputDTO.java
@@ -0,0 +1,14 @@
+package com.algaworks.algafood.api.model.out;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PermissaoOutputDTO {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+
+}
