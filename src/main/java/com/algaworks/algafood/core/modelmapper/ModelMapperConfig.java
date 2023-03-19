b3c0125fda37b4df64cfa5394e7dd67ed5e086c8
Idevaldo Neto <inetomec@yahoo.com.br>
2023-03-19 17:28:47 -0300

    fix: Alterado o ModelConfigMapper para que o o Estado dentro do objeto Cidade seja mapeado adequadamente
    Aula 12.19. Desafio: Implementando os endpoints de consulta de pedidos

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -1,8 +1,9 @@
 package com.algaworks.algafood.core.modelmapper;
 
-import com.algaworks.algafood.api.model.out.RestauranteOutputDTO;
-import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.api.model.out.EnderecoOutputDTO;
+import com.algaworks.algafood.domain.model.Endereco;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,11 +21,18 @@ public class ModelMapperConfig {
     public ModelMapper modelMapper() {
         final ModelMapper modelMapper = new ModelMapper();
 
-        modelMapper.createTypeMap(Restaurante.class, RestauranteOutputDTO.class)
-                .addMapping(Restaurante::getTaxaFrete, RestauranteOutputDTO::setPrecoFrete)
-                .<String>addMapping(
-                src -> src.getEndereco().getCidade().getEstado().getNome(),
-                (dest, value) -> dest.getEndereco().getCidade().setEstado(value));
+//        modelMapper.createTypeMap(Restaurante.class, RestauranteOutputDTO.class)
+//                .addMapping(Restaurante::getTaxaFrete, RestauranteOutputDTO::setPrecoFrete)
+//                .<String>addMapping(
+//                src -> src.getEndereco().getCidade().getEstado().getNome(),
+//                (dest, value) -> dest.getEndereco().getCidade().setEstado(value));
+
+        final TypeMap<Endereco, EnderecoOutputDTO> enderecoToEnderecoModelTypeMap = modelMapper
+                .createTypeMap(Endereco.class, EnderecoOutputDTO.class);
+
+        enderecoToEnderecoModelTypeMap.<String>addMapping(
+                enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+                (enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value));
 
         return modelMapper;
     }
