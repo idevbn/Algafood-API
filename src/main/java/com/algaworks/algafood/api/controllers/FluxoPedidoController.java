09073225670a439ed71748a869a79794975e31be
Idevaldo Neto <inetomec@yahoo.com.br>
2023-03-21 00:06:57 -0300

    feat: Criado controlado que manipula as requisições de fluxo de um Pedido
    Foi implementado, inicialmente, o endpoint que confirma um Pedido
    
    Aula 12.22. Implementando endpoint de transição de status de pedidos

---
+++ src/main/java/com/algaworks/algafood/api/controllers/FluxoPedidoController.java
@@ -0,0 +1,34 @@
+package com.algaworks.algafood.api.controllers;
+
+import com.algaworks.algafood.domain.service.FluxoPedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/pedidos/{id}")
+public class FluxoPedidoController {
+
+    private final FluxoPedidoService pedidoService;
+
+    @Autowired
+    public FluxoPedidoController(final FluxoPedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+
+    @PutMapping(value = "/confirmacao")
+    public ResponseEntity<Void> confirmar(@PathVariable("id") final Long id) {
+        this.pedidoService.confirmar(id);
+
+        final ResponseEntity<Void> response = ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
+
+        return response;
+    }
+
+}
