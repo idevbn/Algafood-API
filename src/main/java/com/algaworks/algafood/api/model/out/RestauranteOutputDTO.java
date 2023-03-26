6d3669113a0dc0b400e9e60903a8c868edfde2f8
Idevaldo Neto <inetomec@yahoo.com.br>
2023-03-26 17:16:33 -0300

    feat: Anotados atributos do RestauranteOutputDTO e da CozinhaOutputDTO com o JsonView
    Dessa forma, os atributos anotados serão exibidos de acordo no retorno do DTO conforme a lógica dos métodos do controlador
    
    Aula 13.1. Fazendo projeção de recursos com @JsonView do Jackson

--- src/main/java/com/algaworks/algafood/api/model/out/RestauranteOutputDTO.java
+++ src/main/java/com/algaworks/algafood/api/model/out/RestauranteOutputDTO.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.api.model.out;
 
+import com.algaworks.algafood.api.model.out.view.RestauranteView;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,9 +11,16 @@ import java.math.BigDecimal;
 @Setter
 public class RestauranteOutputDTO {
 
+    @JsonView({RestauranteView.Resumo.class, RestauranteView.ApenasNome.class})
     private Long id;
+
+    @JsonView({RestauranteView.Resumo.class, RestauranteView.ApenasNome.class})
     private String nome;
+
+    @JsonView(RestauranteView.Resumo.class)
     private BigDecimal precoFrete;
+
+    @JsonView(RestauranteView.Resumo.class)
     private CozinhaOutputDTO cozinha;
     private Boolean ativo;
     private Boolean aberto;
