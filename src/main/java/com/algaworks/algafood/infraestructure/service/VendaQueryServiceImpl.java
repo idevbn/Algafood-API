78dda8c2a9dede51aed637b0a1f84d0837d547cd
Idevaldo Neto <inetomec@yahoo.com.br>
2023-04-02 21:59:44 -0300

    feat: Implementado o método consultarVendasDiarias
    Aula 13.14. Implementando consulta com dados agregados de vendas diárias

--- src/main/java/com/algaworks/algafood/infraestructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infraestructure/service/VendaQueryServiceImpl.java
@@ -1,18 +1,47 @@
 package com.algaworks.algafood.infraestructure.service;
 
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
+import com.algaworks.algafood.domain.model.Pedido;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.service.VendaQueryService;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.*;
+import java.time.LocalDate;
 import java.util.List;
 
-@Service
+@Repository
 public class VendaQueryServiceImpl implements VendaQueryService {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @Override
-    public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filter) {
-        return null;
+    public List<VendaDiaria> consultarVendasDiarias(final VendaDiariaFilter filter) {
+        final CriteriaBuilder builder = this.manager.getCriteriaBuilder();
+        final CriteriaQuery<VendaDiaria> query = builder.createQuery(VendaDiaria.class);
+        final Root<Pedido> root = query.from(Pedido.class);
+
+        final Expression<LocalDate> functionDateDataCriacao = builder
+                .function("date", LocalDate.class, root.get("dataCriacao"));
+
+        final CompoundSelection<VendaDiaria> selection = builder.construct(
+                VendaDiaria.class,
+                functionDateDataCriacao,
+                builder.count(root.get("id")),
+                builder.sum(root.get("valorTotal"))
+        );
+
+        query.select(selection);
+        query.groupBy(functionDateDataCriacao);
+
+        final List<VendaDiaria> vendasDiarias = this.manager
+                .createQuery(query)
+                .getResultList();
+
+        return vendasDiarias;
     }
 
 }
