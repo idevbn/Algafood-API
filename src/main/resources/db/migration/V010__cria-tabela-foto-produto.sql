3c426b17d354bc2c3331392b4de6e1ba68bb8af5
Idevaldo Neto <inetomec@yahoo.com.br>
2023-04-27 23:54:23 -0300

    chore: Adicionada migration que cria tabela t_foto_produto
    Aula 14.5. Mapeando entidade FotoProduto e relacionamento um-para-um

---
+++ src/main/resources/db/migration/V010__cria-tabela-foto-produto.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tb_foto_produto (
+    produto_id BIGINT NOT NULL,
+    nome_arquivo VARCHAR(150) NOT NULL,
+    descricao VARCHAR(150),
+    content_type VARCHAR(80) NOT NULL,
+    tamanho INT NOT NULL,
+
+    PRIMARY KEY (produto_id),
+    CONSTRAINT fk_foto_produto_produto FOREIGN KEY (produto_id) REFERENCES tb_produto (id)
+) engine=InnoDB default charset=utf8;
