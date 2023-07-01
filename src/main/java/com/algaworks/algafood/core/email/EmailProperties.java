a9af31e53b7e05339f312aa8cdd26af22a78fd31
Idevaldo Neto <inetomec@yahoo.com.br>
2023-06-30 23:17:04 -0300

    chore: Criada classe de configuração de propriedades do email
    Aula: 15.3. Implementando o serviço de infraestrutura de envio de e-mails com Spring

---
+++ src/main/java/com/algaworks/algafood/core/email/EmailProperties.java
@@ -0,0 +1,23 @@
+package com.algaworks.algafood.core.email;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@Component
+@Validated
+@ConfigurationProperties(prefix = "algafood.email")
+public class EmailProperties {
+
+    @NotBlank
+    private String remetente;
+
+}
+
+
