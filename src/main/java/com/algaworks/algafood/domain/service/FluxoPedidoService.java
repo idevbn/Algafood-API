0d4d9df9d7b20fa269dc84c22647b30bb3fb6d5b
Idevaldo Neto <inetomec@yahoo.com.br>
2023-07-03 00:03:13 -0300

    feat: Adicionada lógica de  envio de email transacional no fluxo do pedido
    Aula: 15.4. Usando o serviço de envio de e-mails na confirmação de pedidos

--- src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -10,10 +10,13 @@ import javax.transaction.Transactional;
 public class FluxoPedidoService {
 
     private final EmissaoPedidoService pedidoService;
+    private final EnvioEmailService envioEmailService;
 
     @Autowired
-    public FluxoPedidoService(final EmissaoPedidoService pedidoService) {
+    public FluxoPedidoService(final EmissaoPedidoService pedidoService,
+                              final EnvioEmailService envioEmailService) {
         this.pedidoService = pedidoService;
+        this.envioEmailService = envioEmailService;
     }
 
     @Transactional
@@ -21,6 +24,14 @@ public class FluxoPedidoService {
         final Pedido pedido = this.pedidoService.buscarOuFalhar(codigo);
 
        pedido.confirmar();
+
+        final EnvioEmailService.Mensagem mensagem = EnvioEmailService.Mensagem.builder()
+                .assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado!")
+                .corpo("O pedido de código " + pedido.getCodigo() + " foi confirmado")
+                .destinatario(pedido.getCliente().getEmail())
+                .build();
+
+       this.envioEmailService.enviar(mensagem);
     }
 
     @Transactional
