6848c1d32b33f95e33c33a76113c8e02c76f41ee
Idevaldo Neto <inetomec@yahoo.com.br>
2023-07-29 05:14:39 -0300

    chore: Adicionado filtro para utilização de Shallow ETags
    Aula: 17.5. Implementando requisições condicionais com Shallow ETags

--- src/main/java/com/algaworks/algafood/core/web/WebConfig.java
+++ src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -1,8 +1,12 @@
 package com.algaworks.algafood.core.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+import javax.servlet.Filter;
 
 /**
  * Classe de configuração global do CORS
@@ -14,6 +18,23 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*");
+    }
+
+    /**
+     * Recebe uma requisição HTTP e momento de informar a resposta HTTP,
+     * é gerado um hash com o cabeçalho ETag.
+     * <p>
+     * É verificado se o hash da resposta coincide com a ETag 'If-None-Match'
+     * do cabeçalho que vem da requisição.
+     * <p>
+     * Se a hash da resposta e o valor da ETag da requisição coincidirem é
+     * retornado um status code HTTP 304 - Not Modified
+     * <p>
+     * Se o hash e a ETag diferirem é criado nova ETag.
+     */
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
     }
 
 }
