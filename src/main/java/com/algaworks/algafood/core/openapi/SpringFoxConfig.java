94a570c146349689feb834d25f57c3bf54155da6
Idevaldo Neto <inetomec@yahoo.com.br>
2023-07-31 21:16:49 -0300

    chore: Adicionada tag na configuração do SpringFox
    Aula: 18.7. Descrevendo tags na documentação e associando com controllers

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -7,6 +7,7 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -19,7 +20,9 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(this.apiInfo())
+                .tags(new Tag("Cidades", "Gerencia as cidades"));
 
         return docket;
     }
