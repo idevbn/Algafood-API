79b1670ded6a00888dcba3be8d58a410cc8a623c
Idevaldo Neto <inetomec@yahoo.com.br>
2023-07-31 21:07:05 -0300

    chore: Adicionadas informações na exibição da documentação
    Aula: 18.6. Descrevendo informações da API na documentação

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -2,8 +2,11 @@ package com.algaworks.algafood.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -19,6 +22,15 @@ public class SpringFoxConfig {
                 .build();
 
         return docket;
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("AlgaFood API")
+                .description("API aberta para clientes e restaurantes")
+                .version("1")
+                .contact(new Contact("AlgaWorks", "https://www.algaworks.com", "contato@algaworks.com"))
+                .build();
     }
 
 }
