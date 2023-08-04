e2d1b722a76ac7832c7e2e7445c125e18a1b6525
Idevaldo Neto <inetomec@yahoo.com.br>
2023-08-03 23:55:44 -0300

    feat: Alterado nome do Schema no Swagger UI usando a annotation @ApiModel
    Aula: 18.10. Descrevendo modelos de representações e suas propriedades
    
    Para alterar o valor exibido no exemplo do Swagger UI para um
    determinado atributo, basta adicionar a esse atributo a annotation
    @ApiModelProperty(example = valor-desejado).

--- src/main/java/com/algaworks/algafood/api/model/out/CidadeOutputDTO.java
+++ src/main/java/com/algaworks/algafood/api/model/out/CidadeOutputDTO.java
@@ -1,10 +1,12 @@
 package com.algaworks.algafood.api.model.out;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@ApiModel(value = "Cidade")
 public class CidadeOutputDTO {
 
     private Long id;
