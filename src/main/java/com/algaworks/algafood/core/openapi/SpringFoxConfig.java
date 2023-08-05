fff6441017b6eb3edd00c67381863b95b46a2c14
Idevaldo Neto <inetomec@yahoo.com.br>
2023-08-05 18:51:22 -0300

    chore: Adicionada configuração de status de resposta de maneira global
    Aula: 18.12. Descrevendo códigos de status de respostas de forma global
    
    OBS: A configuração foi criada inicialmente para os métodos com HttpStatus GET

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -3,15 +3,23 @@ package com.algaworks.algafood.core.openapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @Import(BeanValidatorPluginsConfiguration.class)
@@ -24,6 +32,8 @@ public class SpringFoxConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
                 .paths(PathSelectors.any())
                 .build()
+                .useDefaultResponseMessages(false)
+                .globalResponses(HttpMethod.GET, globalGetResponseMessages())
                 .apiInfo(this.apiInfo())
                 .tags(new Tag("Cidades", "Gerencia as cidades"));
 
@@ -37,6 +47,24 @@ public class SpringFoxConfig {
                 .version("1")
                 .contact(new Contact("AlgaWorks", "https://www.algaworks.com", "contato@algaworks.com"))
                 .build();
+    }
+
+    private List<Response> globalGetResponseMessages() {
+
+        final Response responseStatusInternalServerError = new ResponseBuilder()
+                .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                .description("Erro interno do Servidor")
+                .build();
+
+        final Response responseStatusNotAcceptable = new ResponseBuilder()
+                .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+                .description("Recurso não possui representação que pode ser aceita pelo consumidor")
+                .build();
+
+        final List<Response> responses = Arrays
+                .asList(responseStatusInternalServerError, responseStatusNotAcceptable);
+
+        return responses;
     }
 
 }
