af67a362fb6a6e06f1c1f16f3c870d45ff7945a3
Idevaldo Neto <inetomec@yahoo.com.br>
2023-08-07 22:58:18 -0300

    feat: Adicionados exemplos para os atributos do ApiError
    Aula: 18.14. Descrevendo o modelo de representação de problema

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiError.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiError.java
@@ -1,6 +1,8 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -15,18 +17,36 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiError {
 
+    @ApiModelProperty(example = "400")
     private Integer status;
+
+    @ApiModelProperty(example = "https://algafood.com.br/dados-invalidos")
     private String type;
+
+    @ApiModelProperty(example = "Dados inválidos")
     private String title;
+
+    @ApiModelProperty(example = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.")
     private String detail;
+
+    @ApiModelProperty(example = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.")
     private String userMessage;
+
+    @ApiModelProperty(example = "2023-03-22T21:22:25.369910132Z")
     private OffsetDateTime timestamp;
+
+    @ApiModelProperty("Lista de bjetos ou campos que geraram o erro (opcional)")
     private List<Object> objects;
 
     @Getter
     @Builder
+    @ApiModel("ApiError Object")
     public static class Object {
+
+        @ApiModelProperty(example = "preço")
         private String name;
+
+        @ApiModelProperty(example = "O preço é obrigatório")
         private String userMessage;
     }
 
