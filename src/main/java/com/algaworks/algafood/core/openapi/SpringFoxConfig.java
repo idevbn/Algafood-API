64ffd32826c16c6e6412e03ef0488f78a706b7fd
Idevaldo Neto <inetomec@yahoo.com.br>
2023-08-13 16:11:47 -0300

    chore: Refatorada configuração de paginação do tipo CozinhaOutputDTO para CozinhasModelOpenApi
    Aula: 18.21. Corrigindo documentação com substituição de Page

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -1,18 +1,22 @@
 package com.algaworks.algafood.core.openapi;
 
-import com.algaworks.algafood.api.controllers.openapi.model.PageableModelOpenApi;
 import com.algaworks.algafood.api.exceptionhandler.ApiError;
+import com.algaworks.algafood.api.model.out.CozinhaOutputDTO;
+import com.algaworks.algafood.api.openapi.model.CozinhasModelOpenApi;
+import com.algaworks.algafood.api.openapi.model.PageableModelOpenApi;
 import com.fasterxml.classmate.TypeResolver;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.*;
+import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.Response;
@@ -45,6 +49,10 @@ public class SpringFoxConfig {
                 .globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
                 .additionalModels(typeResolver.resolve(ApiError.class))
                 .directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
+                .alternateTypeRules(AlternateTypeRules.newRule(
+                        typeResolver.resolve(Page.class, CozinhaOutputDTO.class),
+                        CozinhasModelOpenApi.class
+                ))
                 .apiInfo(this.apiInfo())
                 .tags(new Tag("Cidades", "Gerencia as cidades"))
                 .tags(new Tag("Grupos", "Gerencia os grupos"));
