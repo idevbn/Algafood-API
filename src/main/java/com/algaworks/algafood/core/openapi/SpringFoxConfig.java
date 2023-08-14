03f0fb3c3d20aec921c430e8bd8986d505964fe9
Idevaldo Neto <inetomec@yahoo.com.br>
2023-08-13 22:34:21 -0300

    chore: Adicionada tag para agrupar as ações de Formas de Pagamento
    Aula: 18.24. Desafio: descrevendo documentação de endpoints de formas de pagamento

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -58,7 +58,8 @@ public class SpringFoxConfig {
                 .apiInfo(this.apiInfo())
                 .tags(new Tag("Cidades", "Gerencia as cidades"))
                 .tags(new Tag("Grupos", "Gerencia os grupos"))
-                .tags(new Tag("Cozinhas", "Gerencia as cozinhas"));
+                .tags(new Tag("Cozinhas", "Gerencia as cozinhas"))
+                .tags(new Tag("Formas de pagamento", "Gerencia as formas de pagamento"));
 
         return docket;
     }
