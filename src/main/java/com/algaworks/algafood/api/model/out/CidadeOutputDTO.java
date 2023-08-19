2115b96993e882b8492bebf34e2f8655458e8060
Idevaldo Neto <inetomec@yahoo.com.br>
2023-08-19 17:31:46 -0300

    feat: Preparados os DTOs de saída de Cidade e Estado para retornar hyperlinks
    Aula: 19.7. Adicionando hypermedia na representação de recurso único com HAL
    
    Também foi adicionado a annotation ApiModelProperty em CidadeOutPutDTO e em
    EstadoOutPutDTO

--- src/main/java/com/algaworks/algafood/api/model/out/CidadeOutputDTO.java
+++ src/main/java/com/algaworks/algafood/api/model/out/CidadeOutputDTO.java
@@ -1,16 +1,22 @@
 package com.algaworks.algafood.api.model.out;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
 
 @Getter
 @Setter
 @ApiModel(value = "Cidade")
-public class CidadeOutputDTO {
+public class CidadeOutputDTO extends RepresentationModel<CidadeOutputDTO> {
 
+    @ApiModelProperty(example = "1")
     private Long id;
+
+    @ApiModelProperty(example = "Parahyba")
     private String nome;
+
     private EstadoOutputDTO estado;
 
 }
--- src/main/java/com/algaworks/algafood/api/model/out/EstadoOutputDTO.java
+++ src/main/java/com/algaworks/algafood/api/model/out/EstadoOutputDTO.java
@@ -1,13 +1,18 @@
 package com.algaworks.algafood.api.model.out;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
 
 @Getter
 @Setter
-public class EstadoOutputDTO {
+public class EstadoOutputDTO extends RepresentationModel<EstadoOutputDTO> {
 
+    @ApiModelProperty(example = "1")
     private Long id;
+
+    @ApiModelProperty(example = "PB")
     private String nome;
 
 }
