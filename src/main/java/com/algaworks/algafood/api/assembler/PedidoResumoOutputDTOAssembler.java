37cd07cd0c2d03f52dc046a4c749e3ecdb2cd045
Idevaldo Neto <inetomec@yahoo.com.br>
2023-09-24 09:55:42 -0300

    refactor: Refatorada a inclusão de links utilizando template variables
    Aula: 19.20. Refatorando construção e inclusão de links em representation model

--- src/main/java/com/algaworks/algafood/api/assembler/PedidoResumoOutputDTOAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/PedidoResumoOutputDTOAssembler.java
@@ -1,15 +1,12 @@
 package com.algaworks.algafood.api.assembler;
 
+import com.algaworks.algafood.api.AlgaLinks;
 import com.algaworks.algafood.api.controllers.PedidoController;
 import com.algaworks.algafood.api.controllers.RestauranteController;
 import com.algaworks.algafood.api.controllers.UsuarioController;
 import com.algaworks.algafood.api.model.out.PedidoResumoOutputDTO;
 import com.algaworks.algafood.domain.model.Pedido;
 import org.modelmapper.ModelMapper;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.TemplateVariable;
-import org.springframework.hateoas.TemplateVariables;
-import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
@@ -21,10 +18,12 @@ public class PedidoResumoOutputDTOAssembler
         extends RepresentationModelAssemblerSupport<Pedido, PedidoResumoOutputDTO> {
 
     private final ModelMapper modelMapper;
+    private final AlgaLinks algaLinks;
 
-    public PedidoResumoOutputDTOAssembler(final ModelMapper modelMapper) {
+    public PedidoResumoOutputDTOAssembler(final ModelMapper modelMapper, final AlgaLinks algaLinks) {
         super(PedidoController.class, PedidoResumoOutputDTO.class);
         this.modelMapper = modelMapper;
+        this.algaLinks = algaLinks;
     }
 
     @Override
@@ -33,23 +32,7 @@ public class PedidoResumoOutputDTOAssembler
                 .createModelWithId(pedido.getId(), pedido);
         this.modelMapper.map(pedido, pedidoResumoOutputDTO);
 
-        final TemplateVariables pageVariables = new TemplateVariables(
-                new TemplateVariable("page", TemplateVariable.VariableType.REQUEST_PARAM),
-                new TemplateVariable("sort", TemplateVariable.VariableType.REQUEST_PARAM),
-                new TemplateVariable("size", TemplateVariable.VariableType.REQUEST_PARAM)
-        );
-
-        final TemplateVariables filtroVariables = new TemplateVariables(
-                new TemplateVariable("codigo", TemplateVariable.VariableType.REQUEST_PARAM),
-                new TemplateVariable("nomerestaurante", TemplateVariable.VariableType.REQUEST_PARAM),
-                new TemplateVariable("nomeCliente", TemplateVariable.VariableType.REQUEST_PARAM),
-                new TemplateVariable("valorTotal", TemplateVariable.VariableType.REQUEST_PARAM)
-        );
-
-        final String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
-
-        pedidoResumoOutputDTO.add(Link.of(UriTemplate.of(pedidosUrl,
-                pageVariables.concat(filtroVariables)), "pedidos"));
+        pedidoResumoOutputDTO.add(this.algaLinks.linkToPedidos());
 
         pedidoResumoOutputDTO.getRestaurante().add(linkTo(methodOn(RestauranteController.class)
                 .buscar(pedido.getRestaurante().getId())).withSelfRel());
