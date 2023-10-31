30c6efb8c48cbf07d9dba3cbda65b347710b8a04
Idevaldo Neto <inetomec@yahoo.com.br>
2023-10-31 10:06:42 -0300

    feat: Criado controlador de entrada com os links de listagem de recursos da API
    Aula: 19.36. Implementando o Root Entry Point da API

---
+++ src/main/java/com/algaworks/algafood/api/controllers/RootEntryPointController.java
@@ -0,0 +1,43 @@
+package com.algaworks.algafood.api.controllers;
+
+import com.algaworks.algafood.api.AlgaLinks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class RootEntryPointController {
+
+    private final AlgaLinks algaLinks;
+
+    @Autowired
+    public RootEntryPointController(final AlgaLinks algaLinks) {
+        this.algaLinks = algaLinks;
+    }
+
+    @GetMapping
+    public RootEntryPointModel root() {
+        final RootEntryPointModel rootEntryPointModel = new RootEntryPointModel();
+
+        rootEntryPointModel
+                .add(this.algaLinks.linkToCozinhas("cozinhas"))
+                .add(this.algaLinks.linkToPedidos("pedidos"))
+                .add(this.algaLinks.linkToRestaurantes("restaurantes"))
+                .add(this.algaLinks.linkToGrupos("grupos"))
+                .add(this.algaLinks.linkToUsuarios("usuarios"))
+                .add(this.algaLinks.linkToPermissoes("permissoes"))
+                .add(this.algaLinks.linkToFormasPagamento("formas-pagamento"))
+                .add(this.algaLinks.linkToEstados("estados"))
+                .add(this.algaLinks.linkToCidades("cidades"));
+
+        return rootEntryPointModel;
+    }
+
+    private static class RootEntryPointModel extends RepresentationModel<RootEntryPointModel> {
+    }
+
+}
