07e47c604cd8e8d6ca32e52ecaf962ff3e41ea10
Idevaldo Neto <inetomec@yahoo.com.br>
2023-11-05 23:22:00 -0300

    feat: Adicionado mapeamento para o input de CidadeInputV2
    Aula: 20.11. Implementando o versionamento da API por Media Type

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -2,6 +2,8 @@ package com.algaworks.algafood.core.modelmapper;
 
 import com.algaworks.algafood.api.v1.model.in.ItemPedidoInputDTO;
 import com.algaworks.algafood.api.v1.model.out.EnderecoOutputDTO;
+import com.algaworks.algafood.api.v2.model.in.CidadeInputDTOV2;
+import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Endereco;
 import com.algaworks.algafood.domain.model.ItemPedido;
 import org.modelmapper.ModelMapper;
@@ -22,6 +24,9 @@ public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper() {
         final ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.createTypeMap(CidadeInputDTOV2.class, Cidade.class)
+                .addMappings(mapper -> mapper.skip(Cidade::setId));
 
 //        modelMapper.createTypeMap(Restaurante.class, RestauranteOutputDTO.class)
 //                .addMapping(Restaurante::getTaxaFrete, RestauranteOutputDTO::setPrecoFrete)
