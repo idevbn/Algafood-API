4e492bc06a3778fee4e284914c352672df9ba455
Idevaldo Neto <inetomec@yahoo.com.br>
2023-11-07 21:04:46 -0300

    feat: Implementado método que intercepta os registros de depreciação
    Aula: 20.18. Depreciando uma versão da API

--- src/main/java/com/algaworks/algafood/core/web/WebConfig.java
+++ src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -1,9 +1,11 @@
 package com.algaworks.algafood.core.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
@@ -14,10 +16,22 @@ import javax.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private final ApiDeprecationHandler apiDeprecationHandler;
+
+    @Autowired
+    public WebConfig(final ApiDeprecationHandler apiDeprecationHandler) {
+        this.apiDeprecationHandler = apiDeprecationHandler;
+    }
+
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*");
+    }
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(this.apiDeprecationHandler);
     }
 
     /**
