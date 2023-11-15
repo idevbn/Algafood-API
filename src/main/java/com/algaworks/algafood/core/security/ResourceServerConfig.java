ce497e11bdaa0d30c77ab1c87a53e6b269d54e0c
Idevaldo Neto <inetomec@yahoo.com.br>
2023-11-15 00:03:28 -0300

    chore: Adicionadas configurações
    Aula: 22.11. Configurando o Resource Server com a nova stack do Spring Security

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,48 +1,22 @@
 package com.algaworks.algafood.core.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user_system")
-                .password(this.passwordEncoder().encode("123456"))
-                .roles("ADMIN")
-                .and()
-                .withUser("user_system2")
-                .password(this.passwordEncoder().encode("123456"))
-                .roles("ADMIN");
-    }
-
-    @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        http.httpBasic()
-                .and()
+        http
                 .authorizeRequests()
-                .antMatchers("/v1/cozinhas/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .csrf().disable();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+                .oauth2ResourceServer()
+                .opaqueToken();
     }
 
 }
