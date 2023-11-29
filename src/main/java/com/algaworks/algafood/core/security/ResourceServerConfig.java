0d63d0ceb111d4aefe381e7e852a8a363b08bb6e
Idevaldo Neto <inetomec@yahoo.com.br>
2023-11-28 22:45:23 -0300

    chore: Adicionada configuração para uso do token jwt
    Aula: 23.6. Configurando o Resource Server para JWT assinado com chave simétrica

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,9 +1,14 @@
 package com.algaworks.algafood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -18,7 +23,18 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 .cors()
                 .and()
                 .oauth2ResourceServer()
-                .opaqueToken();
+                .jwt();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        final SecretKeySpec secretKey =
+                new SecretKeySpec("aa27b16ae5be4cdf90d9c37a35f9298a".getBytes(),
+                        "HmacSHA256");
+
+        return NimbusJwtDecoder
+                .withSecretKey(secretKey)
+                .build();
     }
 
 }
