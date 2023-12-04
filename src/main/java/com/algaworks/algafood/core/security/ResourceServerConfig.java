ee55ee394d31d4e3f50e52f33294f8ca0a87f06a
Idevaldo Neto <inetomec@yahoo.com.br>
2023-12-03 23:42:12 -0300

    chore: Removido bean que utilizava chave simétrica
    Aula: 23.12. Configurando a validação de JWT no Resource Server com a chave pública

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,14 +1,9 @@
 package com.algaworks.algafood.core.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-
-import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -24,17 +19,6 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .oauth2ResourceServer()
                 .jwt();
-    }
-
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        final SecretKeySpec secretKey =
-                new SecretKeySpec("aa27b16ae5be4cdf90d9c37a35f9298a".getBytes(),
-                        "HmacSHA256");
-
-        return NimbusJwtDecoder
-                .withSecretKey(secretKey)
-                .build();
     }
 
 }
