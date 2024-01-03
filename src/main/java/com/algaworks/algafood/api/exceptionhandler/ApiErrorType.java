e42cba46e498edea3c3f9b098b8908fbc6b6c111
Idevaldo Neto <inetomec@yahoo.com.br>
2024-01-02 22:51:23 -0300

    chore: Adicionado tipo de erro específico para o acesso negado
    Aula: 23.22. Desafio: tratando AccessDeniedException no ExceptionHandler

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiErrorType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiErrorType.java
@@ -14,7 +14,8 @@ public enum ApiErrorType {
     MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
     PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
     ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
-    DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos");
+    DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos"),
+    ACESSO_NEGADO("/acesso-negado", "Acesso Negado");
 
     private String title;
     private String uri;
