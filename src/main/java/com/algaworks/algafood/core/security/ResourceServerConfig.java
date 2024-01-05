48110e0914e37705d5f56a6955a879653301b6e8
Idevaldo Neto <inetomec@yahoo.com.br>
2024-01-04 22:55:07 -0300

    chore: Adicionadas granted authorities dos escopos no ResourceServer
    Aula: 23.25. Carregando Granted Authorities dos escopos do OAuth2 no Resource Server

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -5,9 +5,12 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -38,10 +41,20 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 authorities = Collections.emptyList();
             }
 
-            return authorities
-                    .stream()
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toSet());
+            final JwtGrantedAuthoritiesConverter scopesAuthoritiesConverter
+                    = new JwtGrantedAuthoritiesConverter();
+
+            final Collection<GrantedAuthority> grantedAuthorities
+                    = scopesAuthoritiesConverter.convert(jwt);
+
+            grantedAuthorities.addAll(
+                    authorities
+                            .stream()
+                            .map(SimpleGrantedAuthority::new)
+                            .collect(Collectors.toSet())
+            );
+
+            return grantedAuthorities;
         });
 
         return jwtAuthenticationConverter;
