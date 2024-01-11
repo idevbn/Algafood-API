064bc8119e293bfcd886417bc77f8343e3afd96c
Idevaldo Neto <inetomec@yahoo.com.br>
2024-01-10 22:56:41 -0300

    chore: Criada lógica de restrição para o recurso de Pedidos usando @PostAuthorize
    Aula: 23.29. Restringindo acessos com @PostAuthorize

--- src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
+++ src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.core.security;
 
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.lang.annotation.ElementType;
@@ -41,6 +42,17 @@ public @interface CheckSecurity {
         @Target(ElementType.METHOD)
         @PreAuthorize("hasAuthority('SCOPE_READ') and isAuthenticated()")
         @interface PodeConsultar { }
+    }
+
+    @interface Pedidos {
+
+        @Retention(RUNTIME)
+        @Target(ElementType.METHOD)
+        @PostAuthorize("hasAuthority('CONSULTAR_PEDIDOS') or "
+                + "@algaSecurity.getUsuarioId() == returnObject.body.cliente.id or "
+                + "@algaSecurity.gerenciaRestaurante(returnObject.body.restaurante.id)")
+        @PreAuthorize("hasAuthority('SCOPE_READ') and isAuthenticated()")
+        @interface PodeBuscar { }
     }
 
 }
