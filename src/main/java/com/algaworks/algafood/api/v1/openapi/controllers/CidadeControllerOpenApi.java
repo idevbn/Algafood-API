822e9b4572d55f4df96abbd1f3723e132664e26e
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-06 16:17:17 -0300

    feat: Adicionada tag na interface que define o controlador de Cidade
    Aula: 26.6. Descrevendo tags na documentação e associando com controllers

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controllers/CidadeControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controllers/CidadeControllerOpenApi.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.v1.openapi.controllers;
 import com.algaworks.algafood.api.v1.model.in.CidadeInputDTO;
 import com.algaworks.algafood.api.v1.model.out.CidadeOutputDTO;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
  *
  * @author Idevaldo Neto <idevbn@gmail.com>
  */
+@Tag(name = "Cidades")
 @SecurityRequirement(name = "security_auth")
 public interface CidadeControllerOpenApi {
 
