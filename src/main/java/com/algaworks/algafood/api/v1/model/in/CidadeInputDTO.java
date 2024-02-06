5f5d5b25f6003192f0d3b0b61ab04c056a30f2e2
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-06 16:57:30 -0300

    feat: Adicionadas descrições nos modelos de representação utilizadas no controller de Cidade
    Aula: 26.9. Descrevendo modelos de representações e suas propriedades

--- src/main/java/com/algaworks/algafood/api/v1/model/in/CidadeInputDTO.java
+++ src/main/java/com/algaworks/algafood/api/v1/model/in/CidadeInputDTO.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.v1.model.in;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +13,7 @@ import javax.validation.constraints.NotNull;
 public class CidadeInputDTO {
 
     @NotBlank
+    @Schema(example = "Parahyba")
     private String nome;
 
     @Valid
--- src/main/java/com/algaworks/algafood/api/v1/model/out/EstadoOutputDTO.java
+++ src/main/java/com/algaworks/algafood/api/v1/model/out/EstadoOutputDTO.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.v1.model.out;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
@@ -10,8 +11,10 @@ import org.springframework.hateoas.server.core.Relation;
 @Relation(collectionRelation = "estados")
 public class EstadoOutputDTO extends RepresentationModel<EstadoOutputDTO> {
 
+    @Schema(example = "1")
     private Long id;
 
+    @Schema(example = "PB")
     private String nome;
 
 }
