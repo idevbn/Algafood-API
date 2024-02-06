8cac4619b5be61b0fbb51bac311cbfb36ced95aa
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-06 20:49:23 -0300

    feat: Adicionados valores exemplo para documentação Swagger na classe ApiError
    Aula: 26.14. Descrevendo o modelo de representação de problema

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiError.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiError.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,29 +13,40 @@ import java.util.List;
  */
 @Getter
 @Builder
+@Schema(name = "ApiError")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiError {
 
+    @Schema(example = "400")
     private Integer status;
 
+    @Schema(example = "https://algafood.com.br/dados-invalidos")
     private String type;
 
+    @Schema(example = "Dados inválidos")
     private String title;
 
+    @Schema(example = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.")
     private String detail;
 
+    @Schema(example = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.")
     private String userMessage;
 
+    @Schema(example = "2024-02-06T19:42:10.902245498Z")
     private OffsetDateTime timestamp;
 
+    @Schema(example = "Lista de objetos ou campos que geraram o erro")
     private List<Object> objects;
 
     @Getter
     @Builder
+    @Schema(name = "ObjectApiError")
     public static class Object {
 
+        @Schema(name = "preco")
         private String name;
 
+        @Schema(name = "O preço é inválido")
         private String userMessage;
     }
 
