189ce22fcbd246d500a4a3f5c9e449e46bd995b2
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-06 16:45:44 -0300

    feat: Adicionada descrição para os parâmetros de entrada do controller de Cidade
    Aula: 26.8. Descrevendo parâmetros de entrada na documentação

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controllers/CidadeControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controllers/CidadeControllerOpenApi.java
@@ -3,6 +3,8 @@ package com.algaworks.algafood.api.v1.openapi.controllers;
 import com.algaworks.algafood.api.v1.model.in.CidadeInputDTO;
 import com.algaworks.algafood.api.v1.model.out.CidadeOutputDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
@@ -22,20 +24,49 @@ public interface CidadeControllerOpenApi {
     ResponseEntity<CollectionModel<CidadeOutputDTO>> listar();
 
     @Operation(summary = "Busca uma cidade por ID")
-    ResponseEntity<CidadeOutputDTO> buscar(final Long id);
+    ResponseEntity<CidadeOutputDTO> buscar(
+            @Parameter(
+                    description = "ID de uma cidade",
+                    example = "1",
+                    required = true
+            )
+            final Long id
+    );
 
     @Operation(
             summary = "Cadastra uma cidade",
             description = "Cadastro de uma cidade necessidade de um estado e um nome válido"
     )
     ResponseEntity<CidadeOutputDTO> adicionar(
+            @RequestBody(
+                    description = "Representação de uma nova cidade",
+                    required = true
+            )
             final CidadeInputDTO cidadeInputDTO
     );
 
     @Operation(summary = "Atualiza uma cidade por ID")
-    ResponseEntity<CidadeOutputDTO> atualizar(final Long id, final CidadeInputDTO cidadeInputDTO);
+    ResponseEntity<CidadeOutputDTO> atualizar(
+            @Parameter(
+                    description = "ID de uma cidade",
+                    example = "1",
+                    required = true
+            ) final Long id,
+            @RequestBody(
+                    description = "Representação de uma cidade com dados atualizados",
+                    required = true
+            )
+            final CidadeInputDTO cidadeInputDTO
+    );
 
     @Operation(summary = "Exclui uma cidade por ID")
-    ResponseEntity<Void> remover(final Long id);
+    ResponseEntity<Void> remover(
+            @Parameter(
+                    description = "ID de uma cidade",
+                    example = "1",
+                    required = true
+            )
+            final Long id
+    );
 
 }
