c64f605b01dab42df2be0f4eca05c52bbfa588b9
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-06 16:37:37 -0300

    feat: Adicionado detalhes de descrição aos endpoints de Cidade
    Aula: 26.7. Descrevendo as operações de endpoints na documentação

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controllers/CidadeControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controllers/CidadeControllerOpenApi.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.v1.openapi.controllers;
 
 import com.algaworks.algafood.api.v1.model.in.CidadeInputDTO;
 import com.algaworks.algafood.api.v1.model.out.CidadeOutputDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
@@ -17,17 +18,24 @@ import org.springframework.http.ResponseEntity;
 @SecurityRequirement(name = "security_auth")
 public interface CidadeControllerOpenApi {
 
+    @Operation(summary = "Lista as cidades")
     ResponseEntity<CollectionModel<CidadeOutputDTO>> listar();
 
+    @Operation(summary = "Busca uma cidade por ID")
     ResponseEntity<CidadeOutputDTO> buscar(final Long id);
 
+    @Operation(
+            summary = "Cadastra uma cidade",
+            description = "Cadastro de uma cidade necessidade de um estado e um nome válido"
+    )
     ResponseEntity<CidadeOutputDTO> adicionar(
             final CidadeInputDTO cidadeInputDTO
     );
 
+    @Operation(summary = "Atualiza uma cidade por ID")
     ResponseEntity<CidadeOutputDTO> atualizar(final Long id, final CidadeInputDTO cidadeInputDTO);
 
-
+    @Operation(summary = "Exclui uma cidade por ID")
     ResponseEntity<Void> remover(final Long id);
 
 }
