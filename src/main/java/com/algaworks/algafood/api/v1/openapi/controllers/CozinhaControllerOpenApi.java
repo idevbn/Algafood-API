8b44fc855174050d8ab9d83ada6249897008ad2c
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-07 09:27:04 -0300

    feat: Adicionado PageableParameter à listagem de cozinhas em CozinhaControllerOpenApi
    Aula: 26.17. Corrigindo documentação com substituição de Pageable

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controllers/CozinhaControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controllers/CozinhaControllerOpenApi.java
@@ -2,6 +2,8 @@ package com.algaworks.algafood.api.v1.openapi.controllers;
 
 import com.algaworks.algafood.api.v1.model.in.CozinhaInputDTO;
 import com.algaworks.algafood.api.v1.model.out.CozinhaOutputDTO;
+import com.algaworks.algafood.core.springdoc.PageableParameter;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.PagedModel;
@@ -10,8 +12,10 @@ import org.springframework.http.ResponseEntity;
 @SecurityRequirement(name = "security_auth")
 public interface CozinhaControllerOpenApi {
 
-
-    ResponseEntity<PagedModel<CozinhaOutputDTO>> listar(final Pageable pageable);
+    @PageableParameter
+    ResponseEntity<PagedModel<CozinhaOutputDTO>> listar(
+            @Parameter(hidden = true) final Pageable pageable
+    );
 
     ResponseEntity<CozinhaOutputDTO> buscar(final Long cozinhaId);
 
