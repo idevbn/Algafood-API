5249e53b7b347a9ba0df54956492440dfe17ff7e
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-07 16:52:27 -0300

    feat: Adicionada descrição de media type
    Aula: 26.22. Descrevendo media type da resposta nos endpoints

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controllers/RestauranteFotoProdutoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controllers/RestauranteFotoProdutoControllerOpenApi.java
@@ -2,6 +2,10 @@ package com.algaworks.algafood.api.v1.openapi.controllers;
 
 import com.algaworks.algafood.api.v1.model.in.FotoProdutoInputDTO;
 import com.algaworks.algafood.api.v1.model.out.FotoProdutoOuputDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
@@ -20,6 +24,28 @@ public interface RestauranteFotoProdutoControllerOpenApi {
     ResponseEntity<Void> removerFoto(final Long restauranteId,
                                      final Long produtoId);
 
+    @Operation(summary = "Busca a foto do produto de um restaurante", responses = {
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = FotoProdutoOuputDTO.class)
+                    ),
+                    @Content(
+                            mediaType = "image/jpeg",
+                            schema = @Schema(type = "string", format = "binary")
+                    ),
+                    @Content(
+                            mediaType = "image/png",
+                            schema = @Schema(type = "string", format = "binary")
+                    )
+            })
+    })
+    ResponseEntity<FotoProdutoOuputDTO> recuperarFoto(
+            final Long restauranteId,
+            final Long produtoId
+    );
+
+    @Operation(hidden = true)
     ResponseEntity<?> servirFoto(final Long restauranteId,
                                  final Long produtoId,
                                  final String acceptHeader
