f6b664d8e9f74a9d963892048680619b1c1c8890
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-06 22:23:47 -0300

    feat: Completado o desafio
    Aula: 26.16. Desafio: descrevendo documentação de endpoints de grupos

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controllers/GrupoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controllers/GrupoControllerOpenApi.java
@@ -2,21 +2,42 @@ package com.algaworks.algafood.api.v1.openapi.controllers;
 
 import com.algaworks.algafood.api.v1.model.in.GrupoInputDTO;
 import com.algaworks.algafood.api.v1.model.out.GrupoOutputDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Grupos")
 @SecurityRequirement(name = "security_auth")
 public interface GrupoControllerOpenApi {
 
+    @Operation(summary = "Lista os grupos")
     ResponseEntity<CollectionModel<GrupoOutputDTO>> listar();
 
-    ResponseEntity<GrupoOutputDTO> buscar(final Long id);
+    @Operation(summary = "Busca um grupo por ID")
+    ResponseEntity<GrupoOutputDTO> buscar(
+            @Parameter(description = "ID de um grupo", example = "1", required = true)
+            final Long id
+    );
 
-    ResponseEntity<GrupoOutputDTO> adicionar(final GrupoInputDTO grupoInputDTO);
+    @Operation(summary = "Cadastra um grupo")
+    ResponseEntity<GrupoOutputDTO> adicionar(
+            @RequestBody(description = "Representação de um novo grupo", required = true)
+            final GrupoInputDTO grupoInputDTO
+    );
 
-    ResponseEntity<GrupoOutputDTO> atualizar(final Long id,
-                                             final GrupoInputDTO grupoInputDTO);
+    @Operation(summary = "Atualiza um grupo por ID")
+    ResponseEntity<GrupoOutputDTO> atualizar(
+            @Parameter(description = "ID de um grupo", example = "1", required = true)
+            final Long id,
+            @RequestBody(description = "Representação de um grupo com os novos dados", required = true)
+            final GrupoInputDTO grupoInputDTO
+    );
+
+    @Operation(summary = "Exclui um grupo por ID")
     ResponseEntity<Void> excluir(final Long id);
 
 }
