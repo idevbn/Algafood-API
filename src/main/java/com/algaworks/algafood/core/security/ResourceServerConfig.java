a882ab4fb64f3ac4844e626834227fab140b37c3
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-09 00:59:23 -0300

    chore: Criada classe de configuração para o Resource Server
    Aula: 27.6. Configurando o Resource Server com token opaco

---
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood.core.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceServerConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/oauth2/**").authenticated()
+                .and()
+                .csrf().disable()
+                .cors().and()
+                .oauth2ResourceServer().opaqueToken();
+
+        return http.build();
+
+    }
+
+}
