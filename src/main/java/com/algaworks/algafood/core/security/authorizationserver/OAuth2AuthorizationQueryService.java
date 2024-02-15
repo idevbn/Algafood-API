71633e723afd87f1c49d232d91721acfbb7c46cc
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-15 14:07:30 -0300

    chore: Criado método que recupera uma lista de clientes autorizados
    Aula: 27.21. Revogando consentimentos e autorizações dos clientes

--- src/main/java/com/algaworks/algafood/core/security/authorizationserver/OAuth2AuthorizationQueryService.java
+++ src/main/java/com/algaworks/algafood/core/security/authorizationserver/OAuth2AuthorizationQueryService.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.core.security.authorizationserver;
 
+import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 
 import java.util.List;
@@ -7,5 +8,8 @@ import java.util.List;
 public interface OAuth2AuthorizationQueryService {
 
     List<RegisteredClient> listClientsWithConsent(final String principalName);
+
+    List<OAuth2Authorization> listAuthorizations(final String principalName,
+                                                 final String clientId);
 
 }
