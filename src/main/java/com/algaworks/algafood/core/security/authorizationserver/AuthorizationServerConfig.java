ed911315729a29ecec3bac21c03b4e56f34f70ac
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-15 19:01:33 -0300

    chore: Adicionadas configurações no AuthorizationServerConfig
    Aula: 28.6. Atualizando o Spring Authorization Server
    
    As configurações precedem a atualização da versão do Spring Auth Server

--- src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -13,9 +13,8 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -25,10 +24,12 @@ import org.springframework.security.oauth2.server.authorization.OAuth2Authorizat
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import java.io.InputStream;
@@ -42,32 +43,37 @@ public class AuthorizationServerConfig {
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityFilterChain authFilterChain(final HttpSecurity http) throws Exception {
-        final OAuth2AuthorizationServerConfigurer<HttpSecurity> authorizationServerConfigurer
-                = new OAuth2AuthorizationServerConfigurer<>();
+        final OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
+                new OAuth2AuthorizationServerConfigurer();
 
-        authorizationServerConfigurer.authorizationEndpoint(customizer -> {
-            customizer.consentPage("/oauth2/consent");
-        });
+        authorizationServerConfigurer.authorizationEndpoint(
+                customizer -> customizer.consentPage("/oauth2/consent")
+        );
 
-        final RequestMatcher endpointsMatcher = authorizationServerConfigurer.getEndpointsMatcher();
+        final RequestMatcher endpointsMatcher = authorizationServerConfigurer
+                .getEndpointsMatcher();
 
-        http
-                .requestMatcher(endpointsMatcher)
-                .authorizeRequests((authorizeRequests) -> {
-            ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)authorizeRequests.anyRequest())
-                    .authenticated();
-        }).csrf((csrf) -> {
-            csrf.ignoringRequestMatchers(endpointsMatcher);
-        }).apply(authorizationServerConfigurer);
+        http.securityMatcher(endpointsMatcher)
+                .authorizeHttpRequests(authorize -> {
+                    authorize.anyRequest().authenticated();
+                })
+                .csrf(csrf -> csrf.ignoringRequestMatchers(endpointsMatcher))
+                .formLogin(Customizer.withDefaults())
+                .exceptionHandling(exceptions -> {
+                    exceptions.authenticationEntryPoint(
+                            new LoginUrlAuthenticationEntryPoint("/login")
+                    );
+                })
+                .apply(authorizationServerConfigurer);
 
-        return http
-                .formLogin(customizer -> customizer.loginPage("/login"))
-                .build();
+        return http.formLogin(customizer -> customizer.loginPage("/login")).build();
     }
 
     @Bean
-    public ProviderSettings providerSettings(final AlgafoodSecurityProperties properties) {
-        return ProviderSettings
+    public AuthorizationServerSettings authorizationServerSettings(
+            final AlgafoodSecurityProperties properties
+    ) {
+        return AuthorizationServerSettings
                 .builder()
                 .issuer(properties.getProviderUrl())
                 .build();
