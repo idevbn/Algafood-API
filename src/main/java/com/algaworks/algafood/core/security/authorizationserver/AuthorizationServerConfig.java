ad3f4c72671e007a60475364a093d5e7900bcbf9
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-15 10:15:39 -0300

    chore: Alteradas configurações no SecurityFilterChain
    Aula: 27.18. Customizando a página de consentimento do OAuth2

--- src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -14,11 +14,14 @@ import org.springframework.core.annotation.Order;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.server.authorization.InMemoryOAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -26,6 +29,7 @@ import org.springframework.security.oauth2.server.authorization.config.ProviderS
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import java.io.InputStream;
 import java.security.KeyStore;
@@ -38,7 +42,23 @@ public class AuthorizationServerConfig {
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityFilterChain authFilterChain(final HttpSecurity http) throws Exception {
-        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        final OAuth2AuthorizationServerConfigurer<HttpSecurity> authorizationServerConfigurer
+                = new OAuth2AuthorizationServerConfigurer<>();
+
+        authorizationServerConfigurer.authorizationEndpoint(customizer -> {
+            customizer.consentPage("/oauth2/consent");
+        });
+
+        final RequestMatcher endpointsMatcher = authorizationServerConfigurer.getEndpointsMatcher();
+
+        http
+                .requestMatcher(endpointsMatcher)
+                .authorizeRequests((authorizeRequests) -> {
+            ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)authorizeRequests.anyRequest())
+                    .authenticated();
+        }).csrf((csrf) -> {
+            csrf.ignoringRequestMatchers(endpointsMatcher);
+        }).apply(authorizationServerConfigurer);
 
         return http
                 .formLogin(customizer -> customizer.loginPage("/login"))
@@ -110,6 +130,11 @@ public class AuthorizationServerConfig {
                 context.getClaims().claim("authorities", authorities);
             }
         };
+    }
+
+    @Bean
+    public OAuth2AuthorizationConsentService consentService() {
+        return new InMemoryOAuth2AuthorizationConsentService();
     }
 
 }
