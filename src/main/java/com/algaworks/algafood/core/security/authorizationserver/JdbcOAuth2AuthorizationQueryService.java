debf556994410a7577e4c98027ced9f84839a03c
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-15 14:08:08 -0300

    chore: Implementado método que recupera uma lista de clientes autorizados
    Aula: 27.21. Revogando consentimentos e autorizações dos clientes

--- src/main/java/com/algaworks/algafood/core/security/authorizationserver/JdbcOAuth2AuthorizationQueryService.java
+++ src/main/java/com/algaworks/algafood/core/security/authorizationserver/JdbcOAuth2AuthorizationQueryService.java
@@ -3,8 +3,11 @@ package com.algaworks.algafood.core.security.authorizationserver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
 import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 
 import java.util.List;
 
@@ -12,24 +15,48 @@ public class JdbcOAuth2AuthorizationQueryService implements OAuth2AuthorizationQ
 
     private final JdbcOperations jdbcOperations;
     private final RowMapper<RegisteredClient> registeredClientRowMapper;
+    private final RowMapper<OAuth2Authorization> oAuth2AuthorizationRowMapper;
 
     private final String LIST_AUTHORIZED_CLIENTS = "SELECT rc.* FROM oauth2_authorization_consent c " +
             "INNER JOIN oauth2_registered_client rc " +
             "ON rc.id = c.registered_client_id " +
             "WHERE c.principal_name = ?";
 
+    private final String LIST_AUTHORIZATION_QUERY = "SELECT a.* FROM oauth2_authorization a " +
+            "INNER JOIN oauth2_registered_client c " +
+            "ON c.id = a.registered_client_id " +
+            "WHERE a.principal_name = ? " +
+            "AND a.registered_client_id  = ? ";
+
     @Autowired
     public JdbcOAuth2AuthorizationQueryService(
-            final JdbcOperations jdbcOperations
-    ) {
+            final JdbcOperations jdbcOperations,
+            final RegisteredClientRepository clientRepository
+            ) {
         this.jdbcOperations = jdbcOperations;
+        this.oAuth2AuthorizationRowMapper
+                = new JdbcOAuth2AuthorizationService.OAuth2AuthorizationRowMapper(clientRepository);
         this.registeredClientRowMapper = new JdbcRegisteredClientRepository.RegisteredClientRowMapper();
     }
 
     @Override
     public List<RegisteredClient> listClientsWithConsent(final String principalName) {
         final List<RegisteredClient> query = this.jdbcOperations
-                .query(LIST_AUTHORIZED_CLIENTS, registeredClientRowMapper, principalName);
+                .query(LIST_AUTHORIZED_CLIENTS, this.registeredClientRowMapper, principalName);
+
+        return query;
+    }
+
+    @Override
+    public List<OAuth2Authorization> listAuthorizations(final String principalName,
+                                                        final String clientId) {
+        final List<OAuth2Authorization> query = this.jdbcOperations
+                .query(
+                        LIST_AUTHORIZATION_QUERY,
+                        this.oAuth2AuthorizationRowMapper,
+                        principalName,
+                        clientId
+                );
 
         return query;
     }
